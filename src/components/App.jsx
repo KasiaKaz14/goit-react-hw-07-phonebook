b4eae87d06ff4d71368504509f7cfe482db7ec2f
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-10-06 20:26:10 +0200

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
-import { addContact, deleteContact, fetchContacts } from 'redux/createAction';
+import { deleteContact, fetchContacts } from 'redux/createAction';
 import { useEffect } from 'react';
 
 export const App = () => {
